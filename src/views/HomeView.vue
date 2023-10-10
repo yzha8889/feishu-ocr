14ef1d7120792c21970a518317fcc7d840c00f58
nemo <122499469@qq.com>
2023-10-10 13:21:53 +0800

    feat: auto load image numbers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,20 @@
 a-space(direction="vertical" fill :size="10")
   a-form.form(:model="form")
     a-space(direction="vertical" :size="10")
-      a-select(v-model="form.attachment" placeholder="选择图片列")
+      a-input(v-model="form.ocrAPI" placeholder="请先填写OCR API，配置方式见下方说明")
+        template(#prefix)
+          icon-robot
+      .info
+        icon-info-circle
+        a(href="https://yellowduck.feishu.cn/docx/Q89SdbfZyoI50RxiwxlcTtP8nkb?from=from_copylink" target="_blank") 查看OCR接口配置文档
+      a-select(v-if="form.ocrAPI" v-model="form.attachment" placeholder="选择图片列")
         template(#prefix)
           icon-file-image
         a-option(v-for="table in attachmentField" :key="table.id" :value="table.id") {{ table.name }}
-      a-select(v-model="form.text" placeholder="选择文本列")
+      a-select(v-if="!!form.attachment" v-model="form.text" placeholder="选择文本列")
         template(#prefix)
           icon-file
         a-option(v-for="view in textField" :key="view.id" :value="view.id") {{ view.name }}
-      a-input(v-model="form.ocrAPI" placeholder="OCR API")
-        template(#prefix)
-          icon-robot
-
   a-space(size="large")
     a-statistic(
       v-if="form.attachment"
@@ -27,7 +29,11 @@ a-space(direction="vertical" fill :size="10")
       title="未识别图片数"
       show-group-separator
       :value="todo.length")
-  a-button(type="primary" :loading="loading" @click="run") 开始识别
+  a-button(
+    type="primary"
+    :disabled="!form.text"
+    :loading="loading"
+    @click="run") 开始识别
 </template>
 <script setup>
   import { bitable, FieldType, IOpenSegmentType } from '@lark-base-open/js-sdk'
@@ -36,12 +42,12 @@ a-space(direction="vertical" fill :size="10")
   import { watch } from 'vue'
 
   // data
+  let table = {}
   const pageSize = 5000
-  let table = null
+  const records = ref([])
   const loading = ref(false)
   const selection = ref({})
   const fieldMetaList = ref([])
-  const records = ref([])
 
   const form = reactive({
     attachment: '',
@@ -52,7 +58,7 @@ a-space(direction="vertical" fill :size="10")
   const attachmentField = computed(() => fieldMetaList.value.filter(meta => meta.type === FieldType.Attachment))
   const textField = computed(() => fieldMetaList.value.filter(meta => meta.type === FieldType.Text))
   const total = computed(() => records.value.filter(record => record.fields[form.attachment]))
-  const todo = computed(() => total.value.filter(record => !record.fields[form.text]))
+  const todo = computed(() => records.value.filter(record => record.fields[form.attachment] && !record.fields[form.text]))
 
   // methods
   const imageToText = async url => {
@@ -79,34 +85,40 @@ a-space(direction="vertical" fill :size="10")
   }
 
   watch(form, async () => {
-    await fetchRecords()
+    fetchRecords()
   })
 
+  watch(
+    () => loading.value && !todo.value.length,
+    async value => {
+      if (value) {
+        loading.value = false
+      }
+    }
+  )
+
   const run = async () => {
+    await fetchRecords()
     loading.value = true
-    for (const recordIndex in todo.value) {
-      let attachmentToken = todo.value[recordIndex].fields[form.attachment][0].token
+    todo.value.forEach(async record => {
+      let attachmentToken = record.fields[form.attachment][0].token
       let attachmentURL = await table.getAttachmentUrl(attachmentToken)
 
-      const texts = await imageToText(attachmentURL)
+      const texts = (await imageToText(attachmentURL)).map(text => ({
+        type: IOpenSegmentType.Text,
+        text: text + '\n',
+      }))
 
-      table.setCellValue(
-        form.text,
-        todo.value[recordIndex].recordId,
-        texts.map(text => ({
-          type: IOpenSegmentType.Text,
-          text: text + '\n',
-        }))
-      )
-
-      todo.value.splice(recordIndex, 1)
-    }
-    loading.value = false
+      table.setCellValue(form.text, record.recordId, texts)
+    })
   }
   // lifecycle
   onMounted(async () => {
     selection.value = await bitable.base.getSelection()
     table = await bitable.base.getTableById(selection.value.tableId)
+    table.onRecordModify((recordId, filedIds) => {
+      fetchRecords()
+    })
     const view = await table.getViewById(selection.value.viewId)
     fieldMetaList.value = await view.getFieldMetaList()
   })
@@ -114,5 +126,18 @@ a-space(direction="vertical" fill :size="10")
 <style lang='stylus' rel='stylesheet/stylus' scoped>
   .form {
     padding: 10px 0;
+  }
+
+  .info {
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 4px;
+    }
+
+    a {
+      color: var(--color-text);
+    }
   }
 </style>
